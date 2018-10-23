9f821063dbbc818ed82eb31e97f8873c34aa2ca2
Joao Greco <junior.greco@hotmail.com>
2018-10-23 09:59:55 -0200

    Add Class ArquivoTXT

---
+++ quine/ArquivoTXT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace quine
+{
+    class ArquivoTXT
+    {
+        public List<Mintermo> LerTXT(string caminhoArquivo)
+        {
+            StreamReader arquivoTXT = new StreamReader(caminhoArquivo);
+
+            List<Mintermo> Mintermos = new List<Mintermo>();
+
+            return Mintermos;
+        }
+    }
+}
